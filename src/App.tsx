5d595dd8d20f3b2d0fbba12a7e7fd44d75b939ec
JPflores20 <pepe.jlfc.16@gmail.com>
2026-01-16 18:06:04 -0600

    implementacion firebase

--- src/App.tsx
+++ src/App.tsx
@@ -5,26 +5,18 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import NotFound from "./pages/NotFound";
-import { uploadData } from "./seed"; // <--- 1. IMPORTAR ESTO
 
 const queryClient = new QueryClient();
 
 const App = () => (
   <QueryClientProvider client={queryClient}>
     <TooltipProvider>
-      {/* 2. AGREGAR ESTE BOTÓN TEMPORAL */}
-      <button 
-        onClick={uploadData} 
-        className="fixed top-0 left-0 z-[9999] bg-red-600 text-white p-4 font-bold border-2 border-white"
-      >
-        CLICK PARA SUBIR DATOS (SOLO 1 VEZ)
-      </button>
-
       <Toaster />
       <Sonner />
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Index />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
